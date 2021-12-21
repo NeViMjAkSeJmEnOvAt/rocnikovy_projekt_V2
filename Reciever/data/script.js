0d211bf56ad4cf1662cc938a5400f6c9a985e191
nevimjaksejmenovat <mikurecka@gmail.com>
2021-12-21 19:07:26 +0100

    dokumentace + úpravy kodu

--- Reciever/data/script.js
+++ Reciever/data/script.js
@@ -1,8 +1,7 @@
 // Vytvoření mapy
-
 let lon = document.getElementById('lon').innerText; //získá souřadnice, které jsou již na stránce vypsané
 let lat = document.getElementById('lat').innerText;
-var map = L.map('map').setView([lat, lon], 12);
+let map = L.map('map').setView([lat, lon], 12);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWNrdWt1cmljZWN6IiwiYSI6ImNreGJ3NDR1ejBwdjEyeHBnMmthamhsajgifQ.GAvd9JUztKQTiwEl5V2JIA', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
@@ -10,17 +9,17 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   tileSize: 512,
   zoomOffset: -1,
 }).addTo(map);
-var marker = L.marker([lat, lon]).addTo(map);
+let marker = L.marker([lat, lon]).addTo(map);
 
 // Update informací
 
-setInterval(function updateDiv() { // div s id #vypisovani se refreshne každých 1000 milisekund (updatování textu)
-  $("#vypisovani").load(window.location.href + " #vypisovani");
-}, 1000);
-
-setInterval(function updateMap() { // Každých 1000 milisekund se znovu vytvoří ukazatel na mapě
+function updateDiv() { // funkce pro obnovování textu a mapy
+  //mapa
   lon = document.getElementById('lon').innerText;
   lat = document.getElementById('lat').innerText;
   map.removeLayer(marker);
   marker = L.marker([lat, lon]).addTo(map);
-}, 1000);
+  //text
+  $("#vypisovani").load(window.location.href + " #vypisovani");
+}
+setInterval(updateDiv(), 1000); //funkce se spustí každých 1000 milisekund
